5ff872ae8ef2edef1cb7ad082ef5e933fc32a853
FedorovSA <itfedorovsa@gmail.com>
2022-04-04 13:09:39 +0300

    Calling a method with the return type.

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -3,8 +3,7 @@ package ru.job4j.oop;
 public class DummyDic {
 
     public String engToRus(String eng) {
-        String unknown = "Неизвестное слово. " + eng;
-        return unknown;
+        return "Неизвестное слово. " + eng;
     }
 
     public static void main(String[] args) {
