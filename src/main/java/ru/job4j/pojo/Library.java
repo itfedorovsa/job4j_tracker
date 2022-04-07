ef40cc8c02e1e9f02f1a06b0f4e4c3f78cb83a12
FedorovSA <itfedorovsa@gmail.com>
2022-04-07 19:25:27 +0300

    Arrays and models

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,33 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book chapter1 = new Book("Part 1", 15);
+        Book chapter2 = new Book("Part 2", 20);
+        Book chapter3 = new Book("Clean Code", 30);
+        Book chapter4 = new Book("Part 4", 50);
+        Book[] library = new Book[4];
+        library[0] = chapter1;
+        library[1] = chapter2;
+        library[2] = chapter3;
+        library[3] = chapter4;
+        for (int index = 0; index < library.length; index++) {
+            Book parts = library[index];
+            System.out.println(parts.getName() + " - " + parts.getPages());
+        }
+        Book temp = library[0];
+        library[0] = library[3];
+        library[3] = temp;
+        for (int index = 0; index < library.length; index++) {
+            Book parts = library[index];
+            System.out.println(parts.getName() + " - " + parts.getPages());
+        }
+        for (int index = 0; index < library.length; index++) {
+            Book parts = library[index];
+            if (parts.getName().equals("Clean Code")) {
+                System.out.println(parts.getName());
+            }
+        }
+    }
+}
