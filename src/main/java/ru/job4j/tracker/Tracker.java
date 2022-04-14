852740838dd3c3d9ff87b7fd5d0794ef0ee479b4
FedorovSA <itfedorovsa@gmail.com>
2022-04-14 14:37:13 +0300

    Tracker - storage

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,32 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[size];
+        int count = 0;
+        for (int index = 0; index < size; index++) {
+            Item value = items[index];
+            if (items[index] != null) {
+                rsl[count] = value;
+                count++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] names = new Item[size];
+        int count = 0;
+        for (int index = 0; index < size; index++) {
+            Item name = items[index];
+            if (key.equals(name.getName())) {
+                names[count] = name;
+                count++;
+            }
+        }
+        return Arrays.copyOf(names, count);
     }
 
     public Item findById(int id) {
