5963b850a63d079785885437bd65b52a304b515a
FedorovSA <itfedorovsa@gmail.com>
2022-04-15 13:24:20 +0300

    Request deleting method. "Tracker.delete"

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -58,14 +58,12 @@ public class Tracker {
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        boolean rsl = index != -1;
-        if (rsl) {
-            int start = index + 1;
-            int length = size - index - 1;
-            System.arraycopy(items, start, items, index, length);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
+            return true;
             }
-        return rsl;
+        return false;
     }
 }
