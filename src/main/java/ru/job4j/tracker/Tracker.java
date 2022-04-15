c17637b0103f6b75aca1d6fc72fd0dbe510e9c32
FedorovSA <itfedorovsa@gmail.com>
2022-04-15 13:16:54 +0300

    Application replacement method. "Tracker.replace"

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -48,12 +48,12 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        item.setId(id);
-        boolean rsl = index != -1;
-        if (rsl) {
+        if (index != -1) {
+            item.setId(id);
             items[index] = item;
+            return true;
         }
-        return rsl;
+        return false;
     }
 
     public boolean delete(int id) {
