9b495d98f1c1c5dacc18209cc112ef8aeb06af09
FedorovSA <itfedorovsa@gmail.com>
2022-04-22 12:24:44 +0300

    Keyword "final" - update

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -23,7 +23,7 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        int a380Engines = name.equals("A380") ? 4 : COUNT_ENGINE;
+        int a380Engines = "A380".equals(name) ? 4 : COUNT_ENGINE;
         System.out.println("Количество двигателей равно: " + a380Engines);
     }
 
