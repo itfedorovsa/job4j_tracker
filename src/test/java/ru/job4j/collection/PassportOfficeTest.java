132b460954d85f1b89368206be409ecd4e348c82
FedorovSA <itfedorovsa@gmail.com>
2022-04-29 19:45:28 +0300

    Passport Office - HashMap - adding tests

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -2,6 +2,11 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -13,5 +18,14 @@ public class PassportOfficeTest {
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void whenPassportDuplicated() {
+        Citizen citizen = new Citizen("1234", "Paul Walker");
+        Citizen citizen1 = new Citizen("1234", "Example");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertFalse(office.add(citizen1));
     }
 }
