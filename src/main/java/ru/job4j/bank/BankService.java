9da56dddad22b57cd71c7757c00209cbdf60ef45
FedorovSA <itfedorovsa@gmail.com>
2022-05-02 18:32:35 +0300

    Bank transfers

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,54 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (!users.get(user).contains(account)) {
+            users.get(user).add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User owner : users.keySet()) {
+            if (owner.getPassport().equals(passport)) {
+                return owner;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            for (Account acc : users.get(user)) {
+                if (acc.getRequisite().equals(requisite)) {
+                    return acc;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount != null && destAccount != null && srcAccount.getBalance() >= amount) {
+            srcAccount.setBalance(srcAccount.getBalance() - amount);
+            destAccount.setBalance(destAccount.getBalance() + amount);
+            return true;
+        }
+        return false;
+    }
+}
