f4e98b2615f7cc8b8218dae71258ceef753d85f0
FedorovSA <itfedorovsa@gmail.com>
2022-05-02 13:54:41 +0300

    Passport Office - HashMap - update

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -7,7 +7,7 @@ public class PassportOffice {
     private Map<String, Citizen> citizens = new HashMap<>();
 
     public boolean add(Citizen citizen) {
-        if (!(citizens.containsKey(citizen.getPassport()))) {
+        if (!citizens.containsKey(citizen.getPassport())) {
             citizens.put(citizen.getPassport(), citizen);
             return true;
         }
