1d6c8d1534139c12e586a92a0e00d5582dd62f93
FedorovSA <itfedorovsa@gmail.com>
2022-05-03 12:03:58 +0300

    Bank transfers - update

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -13,9 +13,11 @@ public class BankService {
     }
 
     public void addAccount(String passport, Account account) {
-        User user = findByPassport(passport);
-        if (!users.get(user).contains(account)) {
-            users.get(user).add(account);
+        if (findByPassport(passport) != null) {
+            User user = findByPassport(passport);
+            if (!users.get(user).contains(account)) {
+                users.get(user).add(account);
+            }
         }
     }
 
