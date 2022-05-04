601ca2ee59323bc839e0e8915cb953d1648997d6
FedorovSA <itfedorovsa@gmail.com>
2022-05-04 12:02:42 +0300

    User sorting by name and age - update

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -16,10 +16,7 @@ public class User implements Comparable<User> {
     @Override
     public int compareTo(User o) {
         int rsl = this.name.compareTo(o.name);
-        if (rsl == 0) {
-            return Integer.compare(this.age, o.age);
-        }
-        return rsl;
+        return (rsl == 0) ? Integer.compare(this.age, o.age) : rsl;
     }
 
     @Override
