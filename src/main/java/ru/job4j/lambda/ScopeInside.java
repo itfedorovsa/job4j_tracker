cf53f2a2c0fb7db76c5c8a4600f1ec0b40aa4463
FedorovSA <itfedorovsa@gmail.com>
2022-05-10 13:30:56 +0300

    Lambda expressions' scope and exceptions - update

--- src/main/java/ru/job4j/lambda/ScopeInside.java
+++ src/main/java/ru/job4j/lambda/ScopeInside.java
@@ -6,10 +6,11 @@ public class ScopeInside {
     public static void main(String[] args) {
         int[] number = {1, 2, 3};
         int total = 0;
-        for (int i = 0; i < number.length; i++) {
-            int num = number[i];
-            total += add(
-                    () -> num
+        for (int num : number) {
+            int total1 = total;
+            total = add(
+                    () ->
+                        total1 + num
             );
         }
         System.out.println(total);
