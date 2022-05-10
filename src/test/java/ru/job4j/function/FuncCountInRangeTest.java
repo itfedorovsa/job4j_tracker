5f65aaf7a93d3dc1cd9795908036c0c14c46736a
FedorovSA <itfedorovsa@gmail.com>
2022-05-10 14:25:42 +0300

    Calculating a function in the range

---
+++ src/test/java/ru/job4j/function/FuncCountInRangeTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.function;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FuncCountInRangeTest {
+
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        FuncCountInRange funcRange = new FuncCountInRange();
+        List<Double> result = funcRange.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenQuadraticFunctionThenQuadraticResults() {
+        FuncCountInRange funcRange = new FuncCountInRange();
+        List<Double> result = funcRange.diapason(2, 5, x -> 2 * x * x + 3 * x + 1);
+        List<Double> expected = Arrays.asList(15D, 28D, 45D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenExponentialFunctionThenExponentialResults() {
+        FuncCountInRange funcRange = new FuncCountInRange();
+        List<Double> result = funcRange.diapason(1, 4, x -> Math.pow(x, 3));
+        List<Double> expected = Arrays.asList(1D, 8D, 27D);
+        assertThat(result, is(expected));
+    }
+
+}
