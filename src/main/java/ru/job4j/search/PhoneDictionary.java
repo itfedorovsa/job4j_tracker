3f2e5a865f8af716a7a48a4a026f8713d5449381
FedorovSA <itfedorovsa@gmail.com>
2022-05-10 15:33:24 +0300

    Higher-order functions

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -3,6 +3,7 @@ package ru.job4j.search;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<>();
@@ -12,15 +13,19 @@ public class PhoneDictionary {
     }
 
     /**
-     * Вернуть список всех пользователей, который содержат key в любых полях.
-     * @param key Ключ поиска.
-     * @return Список подощедщих пользователей.
+     * Return a list of all users that contain the key in any fields.
+     * @param key Search key.
+     * @return Returns a list of eligible users.
      */
     public ArrayList<Person> find(String key) {
+        Predicate<Person> name = s -> s.getName().contains(key);
+        Predicate<Person> surname = s -> s.getSurname().contains(key);
+        Predicate<Person> phone = s -> s.getPhone().contains(key);
+        Predicate<Person> address = s -> s.getAddress().contains(key);
+        Predicate<Person> combine = name.or(surname).or(phone).or(address);
         ArrayList<Person> result = new ArrayList<>();
-       for (Person person : persons) {
-            if (person.getName().contains(key) || person.getSurname().contains(key) || person.getPhone().contains(key)
-            || person.getAddress().contains(key)) {
+        for (Person person : persons) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
