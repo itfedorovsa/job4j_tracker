e0b80fd4fe2b82b10edb065fc9779ec4fea4f693
FedorovSA <itfedorovsa@gmail.com>
2022-05-10 12:01:20 +0300

    Functional Interfaces - update

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -7,37 +7,30 @@ import java.util.Map;
 import java.util.function.*;
 
 public class FunctionalInterfaces {
-    Map<Integer, String> map = new HashMap<>();
-    BiConsumer<Integer, String> biCon = (i, s) -> map.put(i, s);
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        BiConsumer<Integer, String> biCon = (i, s) -> map.put(i, s);
+            biCon.accept(1, "one");
+            biCon.accept(2, "two");
+            biCon.accept(3, "three");
+            biCon.accept(4, "four");
+            biCon.accept(5, "five");
+            biCon.accept(6, "six");
+            biCon.accept(7, "seven");
 
-    {
-        biCon.accept(1, "one");
-        biCon.accept(2, "two");
-        biCon.accept(3, "three");
-        biCon.accept(4, "four");
-        biCon.accept(5, "five");
-        biCon.accept(6, "six");
-        biCon.accept(7, "seven");
-    }
-
-    BiPredicate<Integer, String> biPred = (i, g) -> i % 2 == 0 || g.length() == 4;
-
-    {
-        for (Integer index : map.keySet()) {
-            if (biPred.test(index, map.get(index))) {
-                System.out.println("key: " + index + " value: " + map.get(index));
+        BiPredicate<Integer, String> biPred = (i, g) -> i % 2 == 0 || g.length() == 4;
+            for (Integer index : map.keySet()) {
+                if (biPred.test(index, map.get(index))) {
+                    System.out.println("key: " + index + " value: " + map.get(index));
+                }
             }
-        }
-    }
 
-    Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
-    Consumer<String> con = v -> System.out.println(v);
-    Function<String, String> func = inStr -> inStr.toUpperCase();
-
-    {
-        for (String s : sup.get()) {
-            con.accept(func.apply(s));
-        }
+        Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
+        Consumer<String> con = v -> System.out.println(v);
+        Function<String, String> func = inStr -> inStr.toUpperCase();
+            for (String s : sup.get()) {
+                con.accept(func.apply(s));
+            }
     }
 }
 
