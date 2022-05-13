2c1150bdf3bd5c419649c7e0ab219e20b34300d2
FedorovSA <itfedorovsa@gmail.com>
2022-05-13 14:24:25 +0300

    Optional - Stream API - update

--- src/test/java/ru/job4j/bank/BankServiceTest.java
+++ src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -23,7 +23,7 @@ public class BankServiceTest {
         BankService bank = new BankService();
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        assertEquals(bank.findByRequisite("34", "5546"), Optional.empty());
+        assertThat(bank.findByRequisite("34", "5546"), is(Optional.empty()));
     }
 
     @Test
