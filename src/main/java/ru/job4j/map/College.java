4242faf9a79da2c41ed153d2d638d2acc6d0c042
FedorovSA <itfedorovsa@gmail.com>
2022-05-13 13:57:34 +0300

    Optional - Bank transfers

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -1,6 +1,7 @@
 package ru.job4j.map;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 public class College {
@@ -10,24 +11,23 @@ public class College {
         this.students = students;
     }
 
-    public Student findByAccount(String account) {
+    public Optional<Student> findByAccount(String account) {
         return students.keySet()
                 .stream()
                 .filter(s -> s.getAccount().equals(account))
-                .findFirst()
-                .orElse(null);
+                .findFirst();
     }
 
-    public Subject findBySubjectName(String account, String name) {
-        Student a = findByAccount(account);
-        if (a != null) {
-            return students.get(a)
+    public Optional<Subject> findBySubjectName(String account, String name) {
+        Optional<Student> student = findByAccount(account);
+        Optional<Subject> subject = Optional.empty();
+        if (student.isPresent()) {
+            subject = students.get(student.get())
                     .stream()
                     .filter(s -> s.getName().equals(name))
-                    .findFirst()
-                    .orElse(null);
+                    .findFirst();
         }
-        return null;
+        return subject;
     }
 
     public static void main(String[] args) {
@@ -38,9 +38,9 @@ public class College {
                 )
         );
         College college = new College(students);
-        Student student = college.findByAccount("000001");
-        System.out.println("Found student: " + student);
-        Subject english = college.findBySubjectName("000001", "English");
-        System.out.println("Mark of the found item: " + english.getScore());
+        Optional<Student> student = college.findByAccount("000001");
+        student.ifPresent(value -> System.out.println("Found student: " + value));
+        Optional<Subject> english = college.findBySubjectName("000001", "English");
+        english.ifPresent(value -> System.out.println("Mark of the found item: " + value.getScore()));
     }
 }
