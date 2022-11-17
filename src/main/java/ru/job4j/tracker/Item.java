3b7db025a44e46bbc3c089d27b8806a0e1745fc6
FedorovSA <itfedorovsa@gmail.com>
2022-11-17 22:10:19 +0300

    Lombok added (update)

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,14 +1,37 @@
 package ru.job4j.tracker;
 
-import lombok.Data;
+import lombok.*;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Item {
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+
+    @Getter
+    @Setter
+    @EqualsAndHashCode.Include
     private int id;
+
+    @Getter
+    @Setter
+    @EqualsAndHashCode.Include
     private String name;
+
+    @Getter
     private LocalDateTime created = LocalDateTime.now();
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
