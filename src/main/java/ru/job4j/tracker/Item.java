2cc9a33e7696501e2e67d8d57fa1fc10da2a3e8f
FedorovSA <itfedorovsa@gmail.com>
2022-11-22 17:59:44 +0300

    Hibernate init

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,9 +2,12 @@ package ru.job4j.tracker;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Entity
+@Table(name = "items")
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,6 +16,8 @@ public class Item {
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
     @Setter
     @EqualsAndHashCode.Include
